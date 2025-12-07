fbc9ba3686234476345b79fd32ceb78ce613ed15
Coder Shivam <shivamkumarsingh63724@gmail.com>
2025-12-07 16:50:54 +0530

    fix vite base for netlify

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // 👈 deploy ke liye most important
+  base: "/", // <--- THIS IS THE FIX
 });
